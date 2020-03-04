211b9da01cb779bcb258aec87d9b9735c097311a
ChiweiChen <chiwei0705@gmail.com>
2020-03-04 19:25:58 +0800

    font_change

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,5 +3,39 @@ class Chapter < ApplicationRecord
     has_and_belongs_to_many :users
     scope :uploaded, -> { where('upload is not null') }
     
-    
+    def has_next
+        @all_chapters=self.book.chapters
+        @order=@all_chapters.index(self)
+        if @order<@all_chapters.size-1 && Chapter.where(id: @all_chapters[@order+1].id)[0].upload!=nil
+            return true
+        else
+            return false
+        end
+    end
+
+    def has_prev
+        @all_chapters=self.book.chapters
+        @order=@all_chapters.index(self)
+        if @order>0 && Chapter.where(id: @all_chapters[@order-1].id)[0].upload!=nil
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_next
+        @all_chapters=self.book.chapters
+        @order=@all_chapters.index(self)
+        if self.has_next
+            return @all_chapters[@order+1].id
+        end
+    end
+
+    def get_prev
+        @all_chapters=self.book.chapters
+        @order=@all_chapters.index(self)
+        if self.has_prev
+            return @all_chapters[@order-1].id
+        end
+    end
 end
